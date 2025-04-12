4ddf40a3a712c73b9459d640b8574e1380b0c9ac
Kiet Tuan <101494254+ktcshust@users.noreply.github.com>
2025-04-12 17:49:51 +0700

    Update kafka_producer.py

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,19 +1,34 @@
-# kafka_producer.py - Sends images to Kafka
-
 from kafka import KafkaProducer
 import os
+import json
 
-KAFKA_TOPIC = "caption_topic"
+KAFKA_TOPIC_IMAGE = "image_topic"
+KAFKA_TOPIC_CAPTION = "caption_topic"
 KAFKA_SERVER = "kafka:9092"
 IMAGE_FOLDER = "/data/images"
+CAPTIONS_FILE = "/data/results.csv"
 
 producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 
+# Đọc dữ liệu caption từ file CSV
+captions = []
+with open(CAPTIONS_FILE, 'r') as f:
+    for line in f.readlines():
+        parts = line.strip().split(',')
+        captions.append({"image": parts[0], "caption": parts[1]})
+
+# Gửi ảnh và caption vào Kafka
 for img_name in os.listdir(IMAGE_FOLDER):
     img_path = os.path.join(IMAGE_FOLDER, img_name)
     with open(img_path, "rb") as img_file:
         img_bytes = img_file.read()
-        producer.send(KAFKA_TOPIC, key=img_name.encode(), value=img_bytes)
+        producer.send(KAFKA_TOPIC_IMAGE, key=img_name.encode(), value=img_bytes)
 
-print("All images sent to Kafka!")
+        # Gửi caption
+        for caption in captions:
+            if caption['image'] == img_name:
+                producer.send(KAFKA_TOPIC_CAPTION, value=json.dumps(caption).encode())
+
+print("Tất cả ảnh và caption đã được gửi vào Kafka!")
 producer.close()
+
