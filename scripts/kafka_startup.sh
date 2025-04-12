fbb20f7fd572266bc2e6bbc5e0c5d010f7baba53
Le Tuan Huy <letuanhuy1301@gmail.com>
2025-04-12 19:14:00 +0700

    update the repo

--- scripts/kafka_startup.sh
+++ scripts/kafka_startup.sh
@@ -3,7 +3,7 @@
 # create kafka topic
 kafka-topics.sh --create --topic image_topic --bootstrap-server kafka:9092 --partitions 3 --replication-factor 1
 
-# wait 30 seconds
-sleep 30
+# # wait 30 seconds
+# sleep 30
 
-python /kafka_producer.py
+# python /kafka_producer.py
